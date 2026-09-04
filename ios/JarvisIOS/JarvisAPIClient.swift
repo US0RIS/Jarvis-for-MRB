5b4deee0e13f3ec4bee21c0ea10fd5cec1dce79d
US0RIS <emmett.dubeck@icloud.com>
2026-09-04 00:15:46 -0700

    Fail fast when Jarvis backend is offline

--- ios/JarvisIOS/JarvisAPIClient.swift
+++ ios/JarvisIOS/JarvisAPIClient.swift
@@ -44,7 +44,9 @@ struct JarvisAPIClient {
 
     func health() async throws -> Bool {
         guard let url = URL(string: baseURL)?.appendingPathComponent("health") else { throw JarvisAPIError.badURL }
-        let (_, response) = try await URLSession.shared.data(from: url)
+        var request = URLRequest(url: url)
+        request.timeoutInterval = 5
+        let (_, response) = try await URLSession.shared.data(for: request)
         guard let http = response as? HTTPURLResponse else { throw JarvisAPIError.badResponse }
         return (200..<300).contains(http.statusCode)
     }
@@ -64,6 +66,7 @@ struct JarvisAPIClient {
                     }
                     var request = URLRequest(url: url)
                     request.httpMethod = "POST"
+                    request.timeoutInterval = 120
                     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
                     request.setValue("application/x-ndjson", forHTTPHeaderField: "Accept")
                     addAuthorization(to: &request)
@@ -114,6 +117,7 @@ struct JarvisAPIClient {
             throw JarvisAPIError.badURL
         }
         var request = URLRequest(url: url)
+        request.timeoutInterval = 5
         addAuthorization(to: &request)
         let (data, response) = try await URLSession.shared.data(for: request)
         try validate(response: response, data: data)
@@ -126,6 +130,7 @@ struct JarvisAPIClient {
         }
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
+        request.timeoutInterval = 90
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.setValue("audio/wav", forHTTPHeaderField: "Accept")
         addAuthorization(to: &request)
@@ -146,6 +151,7 @@ struct JarvisAPIClient {
             throw JarvisAPIError.badURL
         }
         var request = URLRequest(url: url)
+        request.timeoutInterval = 10
         addAuthorization(to: &request)
 
         let (data, response) = try await URLSession.shared.data(for: request)
@@ -159,6 +165,7 @@ struct JarvisAPIClient {
         }
         var request = URLRequest(url: url)
         request.httpMethod = "PUT"
+        request.timeoutInterval = 10
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         addAuthorization(to: &request)
         request.httpBody = try JSONSerialization.data(withJSONObject: ["addresses": addresses])
@@ -172,6 +179,7 @@ struct JarvisAPIClient {
         guard let url = URL(string: baseURL)?.appendingPathComponent(path) else { throw JarvisAPIError.badURL }
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
+        request.timeoutInterval = 120
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         addAuthorization(to: &request)
         request.httpBody = try JSONSerialization.data(withJSONObject: body)

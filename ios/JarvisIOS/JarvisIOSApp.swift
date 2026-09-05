aafff549bfd9ccac430e319f3ed5dce638e6fb3a
US0RIS <emmett.dubeck@icloud.com>
2026-09-05 13:10:28 -0700

    Wire private known-people recognizer into iPhone app

--- ios/JarvisIOS/JarvisIOSApp.swift
+++ ios/JarvisIOS/JarvisIOSApp.swift
@@ -8,6 +8,7 @@ struct JarvisIOSApp: App {
     @StateObject private var persistentPresence: PersistentPresenceController
     @StateObject private var meetingCapture: MeetingCaptureController
     @StateObject private var frontendIntelligence: FrontendIntelligenceController
+    @StateObject private var knownPeople: KnownPeopleController
 
     init() {
         do {
@@ -20,12 +21,14 @@ struct JarvisIOSApp: App {
         let presence = PersistentPresenceController(appModel: model)
         let meeting = MeetingCaptureController(appModel: model)
         let frontend = FrontendIntelligenceController(appModel: model)
+        let people = KnownPeopleController()
         frontend.attach(persistentPresence: presence, meetingCapture: meeting)
 
         _appModel = StateObject(wrappedValue: model)
         _persistentPresence = StateObject(wrappedValue: presence)
         _meetingCapture = StateObject(wrappedValue: meeting)
         _frontendIntelligence = StateObject(wrappedValue: frontend)
+        _knownPeople = StateObject(wrappedValue: people)
     }
 
     var body: some Scene {
@@ -35,9 +38,11 @@ struct JarvisIOSApp: App {
                 .environmentObject(persistentPresence)
                 .environmentObject(meetingCapture)
                 .environmentObject(frontendIntelligence)
+                .environmentObject(knownPeople)
                 .task {
                     await persistentPresence.start()
                     await frontendIntelligence.start()
+                    await knownPeople.start(appModel: appModel)
                 }
         }
     }

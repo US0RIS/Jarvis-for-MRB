1b80f2092d77c71d7d977c3b0b9899a845848c5e
US0RIS <emmett.dubeck@icloud.com>
2026-09-17 21:24:34 -0700

    Require REAL Agency gate sessions in strict diagnostics

--- jarvis_mrb/world_diagnostics.py
+++ jarvis_mrb/world_diagnostics.py
@@ -13,7 +13,8 @@ _AGENCY_REQUIRED_TABLES = {
     "agency_plans", "agency_steps", "agency_runtime_state", "agency_settings", "agency_runtime_boots",
     "agency_deliberations", "agency_deliberation_workers", "agency_attention_events",
     "agency_capability_gaps", "agency_decision_cases", "agency_decision_branches",
-    "agency_self_model_entries", "agency_real_gate_receipts", "agency_release_validation_runs",
+    "agency_self_model_entries", "agency_real_gate_sessions",
+    "agency_real_gate_receipts", "agency_release_validation_runs",
 }
 _REQUIRED_TABLES = {
     "entities", "external_ids", "aliases", "events", "event_entities", "beliefs", "commitments",
